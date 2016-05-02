c3fac337428f6cb0a2b1a7e91c7bbb0e0b50c93f
emi yamamoto <e2w1991@gmail.com>
2016-05-02 09:32:59 +0900

    Create update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(user_params)
+      redirect_to controller: :prototypes, action: :index
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :member, :profile, :works)
   end
 end
