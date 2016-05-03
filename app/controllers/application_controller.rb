3c090bae5f65faf01e3cf6c706aded17705463dc
emi yamamoto <e2w1991@gmail.com>
2016-05-03 19:09:56 +0900

    Add strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:name, :member, :profile, :works)
+    devise_parameter_sanitizer.for(:account_update).push(:name, :member, :profile, :works)
   end
 end
