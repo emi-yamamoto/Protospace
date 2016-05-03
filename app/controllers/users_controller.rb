d67135744b640e82bfdd4c5c221ad943ba738cee
emi yamamoto <e2w1991@gmail.com>
2016-05-03 19:31:39 +0900

    Add set_user action to avoid repeating same code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,30 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :show]
+
   def index
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
     user = User.find(params[:id])
     if user.id == current_user.id
       user.update(user_params)
-      redirect_to controller: :prototypes, action: :index
+      redirect_to :root
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :member, :profile, :works)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
