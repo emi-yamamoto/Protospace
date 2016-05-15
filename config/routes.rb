da61a8e5ac1c614f2d5aca46ac7a47ef0e00ecdb
emi yamamoto <e2w1991@gmail.com>
2016-05-15 17:15:34 +0900

    Add code to avoid creating unnecessary actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes
+  resources :prototypes, only: [:index, :new, :show, :create]
   resources :users, only: [:index, :edit, :show, :update]
 end
