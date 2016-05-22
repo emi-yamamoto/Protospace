fd5fefe4ce68a546af7c82ff0a66a7408b69eedf
emi yamamoto <e2w1991@gmail.com>
2016-05-22 09:37:19 +0900

    Add codes for setting default image in case users didn't upload image files

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,6 +6,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+   def default_url
+      "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+   end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
