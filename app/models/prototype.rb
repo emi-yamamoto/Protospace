808302297cc8f9307f59ee4d1576bc541aae0b6d
emi yamamoto <e2w1991@gmail.com>
2016-05-22 11:05:05 +0900

    Add codes for relating main image to prototype

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,8 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images, dependent: :destroy
+  has_one :image, -> { where(status: 0) }
+
   accepts_nested_attributes_for :images
 
   validates :catch_copy,
