87e89abdcd853efef985227d0ac413e8b80340d8
emi yamamoto <e2w1991@gmail.com>
2016-05-24 12:54:09 +0900

    Add edit and destroy action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :move_to_index, except: :index
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user, :main_image).order('created_at DESC').page(params[:page]).per(5)
@@ -22,8 +23,25 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @sub_images = @prototype.images.sub
+  end
+
+  def edit
+    @main_image = @prototype.main_image
+    @sub_images = @prototype.sub_images
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to root_path, notice: 'Edited prototype successfully'
+    else
+      redirect_to edit_prototype_path, alert: "All forms can't be blank"
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path, notice: 'Deleted prototype successfully'
   end
 
   def move_to_index
@@ -39,5 +57,9 @@ class PrototypesController < ApplicationController
       :user_id,
       images_attributes: [:id, :name, :status]
     )
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
