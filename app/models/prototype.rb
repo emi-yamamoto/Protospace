e9699e5de720bbb7008e62dd4d6baddbafcfc44d
emi yamamoto <e2w1991@gmail.com>
2016-05-24 12:57:24 +0900

    Add associations to receive data of sub images

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,8 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images, dependent: :destroy
-  has_one :main_image, -> { where(status: Image.statuses[:main]) }, class_name: "Image"
+  has_one  :main_image, -> { where(status: Image.statuses[:main]) }, class_name: "Image"
+  has_many :sub_images, -> { where(status: Image.statuses[:sub]) }, class_name: "Image"
 
   accepts_nested_attributes_for :images
 
