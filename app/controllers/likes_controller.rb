7354f1f772dbd4fc2618dfa716344efa50ca9974
emi yamamoto <e2w1991@gmail.com>
2016-06-02 10:52:21 +0900

    Modify codes by using associations

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,20 @@
 class LikesController < ApplicationController
+
+  before_action :set_prototype, only: [:create, :destroy]
+
   def create
-    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototype = Prototype.find(params[:prototype_id])
+    @like = @prototype.likes.create(user_id: current_user.id)
+    @likes = @prototype.likes
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-    like.destroy
-    @likes = Like.where(prototype_id: params[:prototype_id])
+    @prototype.likes.find_by(user_id: current_user.id).destroy
+    @likes = @prototype.likes
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  private
+  def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
 end
