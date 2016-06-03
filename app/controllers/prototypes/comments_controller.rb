998f4514b4dff8b712a0cbf182299656aa2d8d07
emi yamamoto <e2w1991@gmail.com>
2016-06-04 07:33:37 +0900

    Add codes to define create and destroy action

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -2,13 +2,22 @@ class Prototypes::CommentsController < ApplicationController
   before_action :set_prototype, only: [:create, :destroy]
 
   def create
+    @comment = @prototype.comments.create(comment_params)
+    @comments = @prototype.comments
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to prototype_path, notice: 'Deleted comment successfully'
   end
 
   private
   def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :prototype_id, :text)
   end
 end
