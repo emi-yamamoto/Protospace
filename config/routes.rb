66347c3ed546330a9cfc8d8487bcc15a93bbab89
emi yamamoto <e2w1991@gmail.com>
2016-06-08 14:21:41 +0900

    Add codes to set routing without prototype_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
+
+  resources :users, only: [:index, :edit, :show, :update]
+  resources :popular, only: :index, module: :prototypes, path: 'prototypes/popular'
+
   resources :prototypes do
     scope module: :prototypes do
       resources :likes, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
     end
   end
-  resources :users, only: [:index, :edit, :show, :update]
 end
