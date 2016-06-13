64a5245474c8191bdc2ef5cc46dc8caccb987c66
emi yamamoto <e2w1991@gmail.com>
2016-06-13 10:51:29 +0900

    Add codes to create index and show action

--- app/controllers/prototypes/tags_controller.rb
+++ app/controllers/prototypes/tags_controller.rb
@@ -1,8 +1,10 @@
 class Prototypes::TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.most_used
+    @tags = ActsAsTaggableOn::Tag.all
   end
 
   def show
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(@tag).includes(:user, :main_image).page(params[:page]).per(20)
   end
 end
