73df94a3152e30082a082816e9cae9f531c91139
emi yamamoto <e2w1991@gmail.com>
2016-06-24 14:46:19 +0900

    Create file for saving images to AWS

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+CarrierWave.configure do |config|
+    config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: config.s3.access_key,
+    aws_secret_access_key: config.s3.secret_key,
+    region: 'us-east-1'
+  }
+
+    case Rails.env
+    when 'development'
+        config.fog_directory  = 'protospace-images'
+        config.asset_host = 'https://s3.amazonaws.com/protospace-images'
+    when 'production'
+        config.fog_directory  = 'protospace-images'
+        config.asset_host = 'https://s3.amazonaws.com/protospace-images'
+    when 'test'
+        config.storage = :file
+    end
+end
