6bf6b1eeffeec4979d60a2c000e8e98f4c2ebb05
emi yamamoto <e2w1991@gmail.com>
2016-07-19 10:07:17 +0900

    Modify codes by using before do not to repeat same codes

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -14,31 +14,39 @@ describe PrototypesController do
   context 'with user login' do
     before { sign_in create(:user) }
     describe 'GET #index' do
+      before do
+        get :index
+      end
+
       it 'assigns the requested prototypes to @prototypes' do
         prototypes = create_list(:prototype, 5)
-        get :index
         expect(assigns(:prototypes)).to include prototype
       end
 
       it 'renders the :index template' do
-        get :index
         expect(response).to render_template :index
       end
     end
 
     describe 'GET #new' do
-      it 'assignes the requested prototype to @prototype' do
+      before do
         get :new
+      end
+
+      it 'assignes the requested prototype to @prototype' do
         expect(assigns(:prototype)).to be_a_new(Prototype)
       end
 
       it 'renders the :new template' do
-        get :new
         expect(response).to render_template :new
       end
     end
 
     describe 'POST #create' do
+        before do
+          post :create, params
+        end
+
       context 'with valid attribtues' do
         it 'saves the new prototype in the database' do
           expect {
@@ -47,17 +55,19 @@ describe PrototypesController do
         end
 
         it 'redirects to root_path' do
-          post :create, params
           expect(response).to redirect_to root_path
         end
 
         it 'shows flash messages to show save the prototype successfully' do
-          post :create, params
           expect(flash[:notice]).to eq 'Saved prototype successfully'
         end
       end
 
       context 'with invalid attribtues' do
+        before do
+          post :create, invalid_params
+        end
+
         it 'does not save the new prototype in the database' do
           expect {
             post :create, invalid_params
@@ -65,106 +75,104 @@ describe PrototypesController do
         end
 
         it 'redirects new_prototype_path' do
-          post :create, invalid_params
           expect(response).to redirect_to new_prototype_path
         end
 
         it 'shows flash messages to show save the prototype unsuccessfully' do
-          post :create, invalid_params
           expect(flash[:alert]).to eq "All forms can't be blank"
         end
       end
     end
 
     describe 'GET #show' do
-      it 'assigns the requested prototype to @prototype' do
+      before do
         get :show, id: prototype
+      end
+
+      it 'assigns the requested prototype to @prototype' do
         expect(assigns(:prototype)).to eq prototype
       end
 
       it 'assigns the requested comment to @comment' do
-        get :show, id: prototype
         expect(assigns(:comment)).to be_a_new(Comment)
       end
 
       it 'assigns like associated with prototype to @like' do
         user = prototype.user
-        get :show, id: prototype
         expect(assigns(:like)).to eq prototype.likes.find_by(user_id: user.id)
       end
 
       it 'renders the :show template' do
-        get :show, id: prototype
         expect(response).to render_template :show
       end
     end
 
     describe 'GET #edit' do
-      it 'assigns the requested prototype to @prototype' do
+      before do
         get :edit, id: prototype
+      end
+
+      it 'assigns the requested prototype to @prototype' do
         expect(assigns(:prototype)).to eq prototype
       end
 
       it 'assigns main_image to @main_image' do
-        get :edit, id: prototype
         expect(assigns(:main_image)).to eq prototype.main_image
       end
 
       it 'assigns sub_images to @sub_images' do
-        get :edit, id: prototype
         expect(assigns(:sub_images)).to eq prototype.images.sub
       end
 
       it 'renders the :edit template' do
-        get :edit, id: prototype
         expect(response).to render_template :edit
       end
     end
 
     describe 'PATCH #update' do
       context 'with valid attributes' do
-        it 'assigns the requested prototype to @prototype' do
+        before do
           patch :update, params
+        end
+
+        it 'assigns the requested prototype to @prototype' do
           expect(assigns(:prototype)).to eq prototype
         end
 
         it 'updates attributes of prototype' do
-          patch :update, params
           prototype.reload
           expect(prototype.title).to eq 'hoge'
         end
 
         it 'redirects to root_path'  do
-          patch :update, params
           expect(response).to redirect_to root_path
         end
 
         it 'shows flash message to show update prototype successfully' do
-          patch :update, params
           prototype.reload
           expect(flash[:notice]).to eq 'Edited prototype successfully'
         end
       end
 
       context 'with invalid attributes' do
-        it 'assigns the requested prototype to @prototype' do
+        before do
           patch :update, invalid_params
+        end
+
+        it 'assigns the requested prototype to @prototype' do
           expect(assigns(:prototype)).to eq prototype
         end
 
         it 'does not save the new prototype' do
-          patch :update, invalid_params
           prototype.reload
           expect(prototype.title).not_to eq 'hoge'
         end
 
         it 'renders the :edit template' do
-          patch :update, invalid_params
           expect(response).to redirect_to edit_prototype_path
         end
 
         it 'shows flash message to show update prototype unsuccessfully' do
-          patch :update, invalid_params
           prototype.reload
           expect(flash[:alert]).to eq "All forms can't be blank"
         end
@@ -172,6 +180,7 @@ describe PrototypesController do
     end
 
     describe 'DELETE #destroy' do
+
       it 'assigns the requested prototype to @prototype' do
         delete :destroy, id: prototype
         expect(assigns(:prototype)).to eq prototype
@@ -184,13 +193,13 @@ describe PrototypesController do
      end
 
      it 'redirects to root_path' do
-       delete :destroy, id: prototype
-       expect(response).to redirect_to root_path
+      delete :destroy, id: prototype
+      expect(response).to redirect_to root_path
      end
 
      it 'shows flash message to show delete prototype successfully' do
-       delete :destroy, id: prototype
-       expect(flash[:notice]).to eq 'Deleted prototype successfully'
+      delete :destroy, id: prototype
+      expect(flash[:notice]).to eq 'Deleted prototype successfully'
      end
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,73 +14,79 @@ describe UsersController do
   describe 'with user login' do
     before { sign_in create(:user) }
     describe 'GET #show' do
-      it "assigns the requested to @user" do
+      before :each do
         get :show, id: user
+      end
+
+      it "assigns the requested to @user" do
         expect(assigns(:user)).to eq user
       end
 
       it "renders the :show template" do
-        get :show, id: user
         expect(response).to render_template :show
       end
     end
 
     describe 'GET #edit' do
-      it "assigns the requested user to @user" do
+      before :each do
         get :edit, id: user
+      end
+
+      it "assigns the requested user to @user" do
         expect(assigns(:user)).to eq user
       end
 
       it "renders the :edit template" do
-        get :edit, id: user
         expect(response).to render_template :edit
       end
     end
 
     describe 'PATCH #update' do
       context 'with valid attributes' do
+        before :each do
+          patch :update, params
+        end
+
         it "locates the requersted @user" do
           patch :update, params
           expect(assigns(:user)).to eq user
         end
 
         it "changes @user's attributes" do
-          patch :update, params
           user.reload
           expect(user.name).to eq("hoge")
         end
 
         it "redirects to user_path" do
-          patch :update, params
           expect(response).to redirect_to root_path
         end
 
         it "sends flash messages" do
-          patch :update, params
           expect(response).to redirect_to root_path
           expect(flash[:notice]).to eq 'Edited profile successfully'
         end
       end
 
       context 'with invalid attributes' do
+        before :each do
+          patch :update, invalid_params
+        end
+
         it "locates the requersted @user" do
           patch :update, invalid_params
           expect(assigns(:user)).to eq user
         end
 
         it "does not changes @user's attributes" do
-          patch :update, invalid_params
           user.reload
           expect(user.name).not_to eq("hoge")
         end
 
         it "redirects to edit_user_path" do
-          patch :update, invalid_params
           expect(response).to redirect_to edit_user_path
         end
 
         it 'shows flash message to show update prototype unsuccessfully' do
-          patch :update, invalid_params
           user.reload
           expect(flash[:alert]).to eq "All forms can't be blank"
         end
