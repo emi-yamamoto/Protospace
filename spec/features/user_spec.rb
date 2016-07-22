08340c3533ea2a9bfc9d84004d71338d2035bcc1
emi yamamoto <e2w1991@gmail.com>
2016-07-22 16:03:51 +0900

    Deleted unnecessary method

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -5,16 +5,6 @@ RSpec.feature 'user', type: :feature do
   given(:login_user) { create(:user) }
   given(:prototype) { build(:prototype, :with_sub_images) }
 
-  def sign_up
-    visit root_path
-    click_on 'Get Started'
-    click_on 'Sign up now'
-    fill_in 'user_name', with: user.name
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'Save'
-  end
-
   def sign_in
     visit root_path
     click_on 'Get Started'
@@ -24,7 +14,13 @@ RSpec.feature 'user', type: :feature do
   end
 
   scenario 'user sign_up' do
-    sign_up
+    visit root_path
+    click_on 'Get Started'
+    click_on 'Sign up now'
+    fill_in 'user_name', with: user.name
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Save'
   end
 
   scenario 'login user and create a new prototype', js: true do
@@ -33,7 +29,7 @@ RSpec.feature 'user', type: :feature do
     fill_in 'prototype_title', with: prototype.title
     attach_file 'prototype[images_attributes][0][name]', "#{Rails.root}/spec/fixtures/img/sample.jpg", visible: false
     1.upto(3) do |i|
-    attach_file "prototype[images_attributes][#{i}][name]", "#{Rails.root}/spec/fixtures/img/sample.jpg", visible: false
+      attach_file "prototype[images_attributes][#{i}][name]", "#{Rails.root}/spec/fixtures/img/sample.jpg", visible: false
     end
     fill_in 'prototype_catch_copy', with: prototype.catch_copy
     fill_in 'prototype_concept', with: prototype.concept
