9c9c16e2035a4dfbfdc2ef0a326cdbd61372aeba
emi yamamoto <e2w1991@gmail.com>
2016-07-22 16:03:17 +0900

    Deleted unnecessary code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,5 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
-    ActionController::Base.asset_host = "http://xxxx.com"
-  Capybara.default_driver = :selenium
   config.include Capybara::DSL
 end
